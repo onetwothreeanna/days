72219be48034a4e7cb1e33c93f91751760877523
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-08-16 10:10:43 -0500

    Initial commit.

---
+++ src/main/java/com/annayoungyeun/days/DaysApplication.java
@@ -0,0 +1,13 @@
+package com.annayoungyeun.days;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysApplication.class, args);
+	}
+}
