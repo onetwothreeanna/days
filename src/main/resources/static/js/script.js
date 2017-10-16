496276495464091a846b805db346eefbb16e5e19
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-10-16 09:33:01 -0500

    Added header image via jumbotron.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,11 +1,10 @@
+$(document).ready(function(){
+    $('html').hide().fadeIn(3000).delay(6000)
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
 
 function initMap() {
     document.getElementById("date").innerHTML = "THIS SHOULD BE A MAP YA IDIOT";
 }
-
-
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-});
-
-$(document).ready(function() { $('html').hide().fadeIn(1500).delay(6000)});
